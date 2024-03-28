8c5c651e5a00b4426850b14264cd4fd27a6f830d
D3Dgroupe <admin@d-e-e-e.fr>
2024-03-28 13:43:41 +0100

    Test conflit

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = create_app()
 if __name__ == '__main__':
     '''
         Point de démarrage en environnement de développement.
-        Test.
+        Test 2.
     '''
 
     # Attention : Bloquant (ne rien exécuter après cette ligne).
