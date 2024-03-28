1b21334f25ea5ad9789845c57ef9e258eb572423
D3Dgroupe <admin@d-e-e-e.fr>
2024-03-28 13:50:37 +0100

    Test 4

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = create_app()
 if __name__ == '__main__':
     '''
         Point de démarrage en environnement de développement.
-        Test 3.
+        Test 4.
     '''
 
     # Attention : Bloquant (ne rien exécuter après cette ligne).
