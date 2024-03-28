7b7d23529dfc751f6406a3c5c2cbebf5658b7f33
newenpoi <lammenschristopher119@hotmail.com>
2024-03-28 13:56:34 +0100

    test 5 recent (conflict resolver)

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = create_app()
 if __name__ == '__main__':
     '''
         Point de démarrage en environnement de développement.
-        Test 3.
+        Test 5-Recent.
     '''
 
     # Attention : Bloquant (ne rien exécuter après cette ligne).
